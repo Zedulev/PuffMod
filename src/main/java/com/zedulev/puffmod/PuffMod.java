f9e0d4b86ab251eea60e4894d9ce2885e837a028
Zedulev <zedulev@outlook.com>
2021-12-06 04:53:25 -0500

    Initial commit

---
+++ src/main/java/com/zedulev/puffmod/PuffMod.java
@@ -0,0 +1,33 @@
+package com.zedulev.puffmod;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(PuffMod.MOD_ID)
+public class PuffMod
+{
+    public static final String MOD_ID = "puffmod";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public PuffMod() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        eventBus.addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
